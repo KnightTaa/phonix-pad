7c5dc2bbb85e7f313edaa92725207a80617d141e
TerrysDev <152247938+TerrysDev@users.noreply.github.com>
2024-06-17 17:24:46 +0530

    all forms done

--- components/UploadForm.jsx
+++ components/UploadForm.jsx
@@ -1,42 +1,39 @@
 "use client";
-import React, { useState } from "react";
-import Link from "next/link";
-import { BiSolidHide, BiSolidShow } from "react-icons/bi";
 import Image from "next/image";
-import Wrapper from "./Wrapper";
+import React, { useState } from "react";
+import { CloudArrowUpIcon } from "@heroicons/react/24/solid";
+import Wrapper from "@/components/Wrapper";
 
 const countries = [
-  { value: "LK", label: "🇱🇰", code: "+94" },
-  { value: "US", label: "🇺🇸", code: "+1" },
-  { value: "IN", label: "🇮🇳", code: "+91" },
-  { value: "UK", label: "🇬🇧", code: "+44" },
-  { value: "CA", label: "🇨🇦", code: "+1" },
-  { value: "AU", label: "🇦🇺", code: "+61" },
-  { value: "DE", label: "🇩🇪", code: "+49" },
-  { value: "FR", label: "🇫🇷", code: "+33" },
-  { value: "JP", label: "🇯🇵", code: "+81" },
-  { value: "CN", label: "🇨🇳", code: "+86" },
-  { value: "BR", label: "🇧🇷", code: "+55" },
-  { value: "RU", label: "🇷🇺", code: "+7" },
-  { value: "MX", label: "🇲🇽", code: "+52" },
-  { value: "AR", label: "🇦🇷", code: "+54" },
-  { value: "TR", label: "🇹🇷", code: "+90" },
-  { value: "PH", label: "🇵🇭", code: "+63" },
-];
+    { value: "LK", label: "🇱🇰", code: "+94" },
+    { value: "US", label: "🇺🇸", code: "+1" },
+    { value: "IN", label: "🇮🇳", code: "+91" },
+    { value: "UK", label: "🇬🇧", code: "+44" },
+    { value: "CA", label: "🇨🇦", code: "+1" },
+    { value: "AU", label: "🇦🇺", code: "+61" },
+    { value: "DE", label: "🇩🇪", code: "+49" },
+    { value: "FR", label: "🇫🇷", code: "+33" },
+    { value: "JP", label: "🇯🇵", code: "+81" },
+    { value: "CN", label: "🇨🇳", code: "+86" },
+    { value: "BR", label: "🇧🇷", code: "+55" },
+    { value: "RU", label: "🇷🇺", code: "+7" },
+    { value: "MX", label: "🇲🇽", code: "+52" },
+    { value: "AR", label: "🇦🇷", code: "+54" },
+    { value: "TR", label: "🇹🇷", code: "+90" },
+    { value: "PH", label: "🇵🇭", code: "+63" },
+  ];
 
-const SignUpForm = () => {
-  const [selectedCountry, setSelectedCountry] = useState(countries[0]);
-  const phonePlaceholder = selectedCountry ? selectedCountry.code : "";
-
-  const [showPassword, setShowPassword] = useState(false);
-  const togglePassword = () => {
-    setShowPassword(!showPassword);
-  };
-  const passwordType = showPassword ? "text" : "password";
-
+const UploadForm = () => {
   const [selectedRadio, setSelectedRadio] = useState("shorts");
   const [bgColor, setBgColor] = useState("bg-yellow-400");
   const [textColor, setTextColor] = useState("text-black");
+
+  const [selectedCountry, setSelectedCountry] = useState(countries[0]);
+  const phonePlaceholder = selectedCountry ? selectedCountry.code : "";
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleRadioChange = (radioId) => {
     setSelectedRadio(radioId);
@@ -59,13 +56,17 @@ const SignUpForm = () => {
   };
   return (
     <section className={`w-full min-h-screen ${bgColor}`}>
-      <Wrapper>
-        <form className="w-full flex flex-col md:flex-col lg:flex-row items-center justify-between gap-10 p-2 sm:p-4">
+      <Wrapper className="w-full min-h-screen flex flex-col items-center justify-center">
+        <form className="w-full flex flex-col md:flex-col lg:flex-row justify-between gap-5 p-2 sm:p-4">
           {/* left side */}
-          <div className="w-full min-h-screen md:w-full lg:w-1/2 flex justify-center flex-col gap-2">
+          <div className="w-full md:w-full lg:w-1/2 flex flex-col gap-2">
+            <h1
+              className={`uppercase text-2xl text-start sm:text-5xl font-semibold ${textColor}`}
+            >
+              Select your project
+            </h1>
+
             <fieldset className="mt-5">
-              {/* className="flex items-center gap-10" */}
-              {/* "grid grid-cols-1 sm:grid-cols-3 gap-5 items-center justify-center" */}
               <div className="flex items-center gap-10">
                 <input
                   id="shorts"
@@ -78,9 +79,7 @@ const SignUpForm = () => {
                 <div
                   onClick={() => handleRadioChange("shorts")}
                   className={`cursor-pointer transition-all duration-300 ${
-                    selectedRadio === "shorts"
-                      ? "w-[100px] md:w-[120px] lg:w-[200px]"
-                      : "w-[80px] md:w-[120px] lg:w-[160px]"
+                    selectedRadio === "shorts" ? "w-[100px] md:w-[120px] lg:w-[200px]" : "w-[80px] md:w-[120px] lg:w-[160px]"
                   }`}
                 >
                   <Image
@@ -99,13 +98,10 @@ const SignUpForm = () => {
                   checked={selectedRadio === "brands"}
                   onChange={() => handleRadioChange("brands")}
                 />
-                {/* "w-[100px]" : "w-[80px]" */}
                 <div
                   onClick={() => handleRadioChange("brands")}
                   className={`cursor-pointer transition-all duration-300 ${
-                    selectedRadio === "brands"
-                      ? "w-[100px] md:w-[120px] lg:w-[200px]"
-                      : "w-[80px] md:w-[120px] lg:w-[160px]"
+                    selectedRadio === "brands" ? "w-[100px] md:w-[120px] lg:w-[200px]" : "w-[80px] md:w-[120px] lg:w-[160px]"
                   }`}
                 >
                   <Image
@@ -127,9 +123,7 @@ const SignUpForm = () => {
                 <div
                   onClick={() => handleRadioChange("products")}
                   className={`cursor-pointer transition-all duration-300 ${
-                    selectedRadio === "products"
-                      ? "w-[100px] md:w-[120px] lg:w-[200px]"
-                      : "w-[80px] md:w-[120px] lg:w-[160px]"
+                    selectedRadio === "products" ? "w-[100px] md:w-[120px] lg:w-[200px]" : "w-[80px] md:w-[120px] lg:w-[160px]"
                   }`}
                 >
                   <Image
@@ -143,27 +137,13 @@ const SignUpForm = () => {
               </div>
             </fieldset>
             <h1
-              className={`uppercase text-xl text-start sm:text-4xl font-semibold mt-10 sm:mt-20 ${textColor}`}
+              className={`uppercase text-xl text-start sm:text-3xl font-semibold mt-10 ${textColor}`}
             >
-              Criteria
+              Select your project
             </h1>
             <p
-              className={`text-base sm:text-lg font-normal mt-5 leading-6 sm:leading-8 ${textColor}`}
+              className={`text-base sm:text-lg font-medium mt-5 leading-6 sm:leading-8 ${textColor}`}
             >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -173,13 +153,12 @@ const SignUpForm = () => {
               sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
           </div>
-
           {/* right side */}
-          <div className="w-full md:w-full lg:w-1/2 py-5 px-2 sm:p-8 mt-5 sm:mt-0 bg-black rounded-tr-none sm:rounded-tr-[20%]">
-            <h1
+          <div className="w-full md:w-full lg:w-1/2 p-4 sm:p-8 mt-5 sm:mt-0 bg-black rounded-tr-none sm:rounded-tr-[20%]">
+          <h1
               className={`uppercase text-xl text-start sm:text-4xl font-semibold mb-10 text-white`}
             >
-              Sign Up
+              Upload
             </h1>
             <div className="space-y-12">
               <div className="grid grid-cols-1 gap-x-6 gap-y-7 sm:grid-cols-6">
@@ -246,7 +225,7 @@ const SignUpForm = () => {
                   </label>
                   <div className="mt-1 relative rounded-md shadow-sm">
                     <div className="absolute inset-y-0 left-0 flex items-center justify-center">
-                      <select
+                    <select
                         id="country"
                         name="country"
                         value={selectedCountry.value}
@@ -279,119 +258,55 @@ const SignUpForm = () => {
                   </div>
                 </div>
                 <div className="col-span-full">
-                  <div className="flex items-center justify-between">
-                    <label
-                      htmlFor="password"
-                      className="block text-sm font-medium leading-6 text-white"
-                    >
-                      Password
-                    </label>
-                    <div className="flex items-center gap-2">
-                      <BiSolidShow
-                        size={20}
-                        className={`${!showPassword ? "text-white" : "hidden"}`}
-                        onClick={togglePassword}
-                      />
-                      <span
-                        className={`${
-                          !showPassword
-                            ? "text-sm font-medium leading-6 text-white"
-                            : "hidden"
-                        }`}
-                      >
-                        Show
-                      </span>
-                      <BiSolidHide
-                        size={20}
-                        className={`${showPassword ? "text-white" : "hidden"}`}
-                        onClick={togglePassword}
-                      />
-                      <span
-                        className={`${
-                          showPassword
-                            ? "text-sm font-medium leading-6 text-white"
-                            : "hidden"
-                        }`}
-                      >
-                        Hide
-                      </span>
-                    </div>
-                  </div>
-                  <div className="mt-2">
-                    <input
-                      type={passwordType}
-                      name="password"
-                      id="password"
-                      autoComplete="+94"
-                      className="block w-full rounded-lg border-0 py-1.5 px-2 outline-none text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                  <label
+                    htmlFor="cover-photo"
+                    className="block text-sm font-medium leading-6 text-white"
+                  >
+                    Upload your File
+                  </label>
+                  <div className="mt-2 flex flex-col items-center justify-center rounded-lg border border-dashed border-white/35 px-6 py-10">
+                    <CloudArrowUpIcon
+                      className="mx-auto h-12 w-12 text-gray-300"
+                      aria-hidden="true"
                     />
+                    <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                      <label
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer rounded-xl font-semibold text-white/75 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-white transition-all duration-200"
+                      >
+                        <span>Browse</span>
+                        <input
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          className="sr-only"
+                        />
+                      </label>
+                      <p className="pl-1 text-white/75">or drag and drop</p>
+                    </div>
+                    <p className="text-xs leading-5 text-white/75">
+                      Supported formats: JPEG, PNG, GIF, MP4, PDF, PSD, AI,
+                      WORD, PPT
+                    </p>
                   </div>
-                  <p className="text-xs mt-1 text-white/80">
-                    Use 8 or more characters with a mix of letters, numbers &
-                    symbols
-                  </p>
+                  <div className="mt-2 flex flex-col gap-4">
+                    <span className="text-sm font-medium text-white">
+                      Uploading - 3/3 files
+                    </span>
+                    <span className="text-sm font-medium text-white">
+                      Uploaded
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
-            <fieldset>
-              <div className="mt-6 space-y-6">
-                <div className="relative flex gap-x-3">
-                  <div className="flex h-6 items-center">
-                    <input
-                      id="terms"
-                      name="terms"
-                      type="checkbox"
-                      className="h-4 w-4 rounded border-gray-300 checked:bg-black checked:text-white"
-                    />
-                  </div>
-                  <div className="text-sm leading-6">
-                    <p className="font-normal text-white">
-                      By creating an account, you agree to our{" "}
-                      <span className="font-medium underline">
-                        Terms of use
-                      </span>{" "}
-                      and{" "}
-                      <span className="font-medium underline">
-                        Privacy Policy
-                      </span>
-                    </p>
-                  </div>
-                </div>
-                <div className="relative flex gap-x-3">
-                  <div className="flex h-6 items-center">
-                    <input
-                      id="condition"
-                      name="condition"
-                      type="checkbox"
-                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                  </div>
-                  <div className="text-sm leading-6">
-                    <p className="font-normal text-white">
-                      By creating an account, I am also consenting to resive SMS
-                      messages and emails, including products new feature
-                      updates, events, and marketing promotions.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </fieldset>
             <div className="mt-6 flex items-center justify-start gap-x-6">
               <button
                 type="button"
-                className="text-sm font-semibold leading-6 bg-primary px-6 py-2 rounded-full text-white hover:bg-primary-hover transition-all duration-200"
+                className="text-sm w-full font-semibold leading-6 bg-primary px-6 py-2 rounded-full text-white hover:bg-primary-hover transition-all duration-200"
               >
-                Sign Up
+                Submit
               </button>
-              <p className="text-xs sm:text-sm text-white/80">
-                Allready have an account?{" "}
-                <Link
-                  href="/sign-in"
-                  className="underline text-white font-semibold"
-                >
-                  Log in
-                </Link>
-              </p>
             </div>
           </div>
         </form>
@@ -400,4 +315,4 @@ const SignUpForm = () => {
   );
 };
 
-export default SignUpForm;
+export default UploadForm;
