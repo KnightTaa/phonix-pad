38cf5f66d1559f713e7f36efc7d70452eb53d21c
TerrysDev <152247938+TerrysDev@users.noreply.github.com>
2024-06-18 14:15:50 +0530

    font added

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,6 @@
 import localfont from "next/font/local";
 import "./globals.css";
 
-// const arial = localfont({
-//   src: [{
-//     path: '../public/fonts/arial.ttf',
-//     // weight: '400',
-//   }],
-//   variable: "--font-arial",
-// })
-
 const arial = localfont({
   src: './fonts/arial.ttf',
 })
@@ -17,7 +9,7 @@ export const metadata = {
   title: "Pad Frontend",
   description: "Test frontend for phonix pad",
 };
-{/* <body className={`${arial.variable}`}>{children}</body> */}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
