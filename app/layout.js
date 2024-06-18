18226370f501c72da718ea508142dbea06a2e447
TerrysDev <152247938+TerrysDev@users.noreply.github.com>
2024-06-18 13:43:15 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,5 @@
-import { Poppins } from "next/font/google";
 import localfont from "next/font/local";
 import "./globals.css";
-
-// const inter = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
 
 const arial = localfont({
   src: [{
@@ -16,7 +13,7 @@ export const metadata = {
   title: "Pad Frontend",
   description: "Test frontend for phonix pad",
 };
-{/* <body className={inter.className}>{children}</body> */}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
